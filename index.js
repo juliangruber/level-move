79957d9ad687b57d01a6e558c64dc04d5d90d29e
dskrepps <dskrepps@gmail.com>
2016-09-08 20:51:54 -0400

    use level-lock

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
+var lock = require('level-lock');
+
 module.exports = move;
 move.install = install;
 
 function move(db, from, to, fn) {
+  
+  var unlock = lock(db, from, 'w');
+  if (!unlock) {
+    var err = new Error('key is write-locked');
+    err.code = err.type = 'LOCKED';
+    return setImmediate(fn, err);
+  }
+  
   db.get(from, function(err, value) {
-    if (err) return fn(err);
+    if (err) {
+      unlock();
+      return fn(err);
+    }
+    
     db.batch([
       { type: 'del', key: from },
       { type: 'put', key: to, value: value }
-    ], fn);
+    ], function(err) {
+      unlock();
+      fn(err);
+    });
   });
 }
 
